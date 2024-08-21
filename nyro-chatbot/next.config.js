32c357cff75f483f267d27cdaeceb70e794e0fb7
Akshay Belhe <abelhe8900@gmail.com>
2024-08-21 15:03:30 -0500

    attempt 7 on timeout error

--- nyro-chatbot/next.config.js
+++ nyro-chatbot/next.config.js
@@ -13,7 +13,8 @@ module.exports = withBundleAnalyzer(
       // Warning: This allows production builds to successfully complete even if
       // your project has ESLint errors.
       ignoreDuringBuilds: true,
-    },  
+    },
+    output:"standalone",
     images: {
       remotePatterns: [
         {
