1cae1b697abfff57ad6f4ab363d5c7039cddcd70
Akshay Belhe <abelhe8900@gmail.com>
2024-08-22 14:09:52 -0500

    fixx attempt 2

--- nyro-chatbot/lib/supabase/client.ts
+++ nyro-chatbot/lib/supabase/client.ts
@@ -1,6 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr"
 
 export const createClient = () =>
+  console.log("Supabase URL client:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("Supabase Anon Key client:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
--- nyro-chatbot/lib/supabase/server.ts
+++ nyro-chatbot/lib/supabase/server.ts
@@ -2,9 +2,16 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr"
 import { cookies } from "next/headers"
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+  
+  console.log("Supabase URL server:", supabaseUrl);
+  console.log("Supabase Anon Key server:", supabaseAnonKey);
+
+  
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl,
+    supabaseAnonKey,
     {
       cookies: {
         get(name: string) {
