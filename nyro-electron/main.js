f4fa1271d51cf860a80c65fb22d7db53507d288a
yoosh2842 <thereqltimel@gmail.com>
2024-09-09 17:33:27 -0500

    back to 0.1.2

--- nyro-electron/main.js
+++ nyro-electron/main.js
@@ -15,8 +15,7 @@ updateElectronApp({
     type: UpdateSourceType.ElectronPublicUpdateService,
     repo: 'trynyro/nyro-app'
   },
-  updateInterval: '10 minutes',
-  logger: require('electron-log')
+  updateInterval: '10 minutes'
 })
 let mainWindow;
 let isProcessingShortcut = false;
